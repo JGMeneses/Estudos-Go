cecde1adac7bf3baf2b83b165c5ff9bf05acc74e
JGMeneses <joaovictorcont15@gmail.com>
2024-06-09 20:00:00 -0300

    feat(exercises): added implementation of the clear function and modified the layout for menus

--- Exercicios/main.go
+++ Exercicios/main.go
@@ -2,34 +2,80 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	questions "projeto001/Exercicios/Lista01/questions"
+	questions02 "projeto001/Exercicios/Lista02/questions02"
 )
 
 func main() {
-	// Call the functions from firstQuest.go and secondQuest.go as needed
-	var choice int
 
-	fmt.Print("Which question do you want to access? \n 1- First question \n 2- Second question")
-	fmt.Print("\n 3- Third question \n 4- Fourth question\n 5- Five question \n 6- Five question")
-	fmt.Print("\n 7- Third question \n 8- Fourth question\n 9- Five question \n 10- Five question\n")
-	fmt.Scan(&choice) // Read user's choice
+	var listaChoice, questionChoice int
 
-	switch choice {
+	fmt.Println("Which list do you want to access?")
+	fmt.Println("1- List 1")
+	fmt.Println("2- List 2")
+
+	fmt.Print("Enter your choice: ")
+	fmt.Scan(&listaChoice)
+	clearScreen()
+	switch listaChoice {
 	case 1:
-		questions.One()
+		fmt.Println("Which question do you want to access from List 1?")
+		fmt.Print("Which question do you want to access? \n 1- First question \n 2- Second question")
+		fmt.Print("\n 3- Third question \n 4- Fourth question\n 5- Five question \n 6- Five question")
+		fmt.Print("\n 7- Third question \n 8- Fourth question\n 9- Five question \n 10- Five question\n")
+		fmt.Scan(&questionChoice) // Read user's choice
+		clearScreen()             // Clear the screen
+		switch questionChoice {
+		case 1:
+			questions.One()
+		case 2:
+			questions.Two()
+		case 3:
+			questions.Three()
+		case 4:
+			questions.Four()
+		case 5:
+			questions.Five()
+		case 6:
+			questions.Six()
+		case 7:
+			questions.Seven()
+		default:
+			fmt.Println("Invalid option!")
+		}
 	case 2:
-		questions.Two()
-	case 3:
-		questions.Three()
-	case 4:
-		questions.Four()
-	case 5:
-		questions.Five()
-	case 6:
-		questions.Six()
-	case 7:
-		questions.Seven()
-	default:
-		fmt.Println("Invalid option!")
+		fmt.Println("Which question do you want to access from List 2?")
+		fmt.Print("Which question do you want to access? \n 1- First question \n 2- Second question")
+		fmt.Print("\n 3- Third question \n 4- Fourth question\n 5- Five question \n 6- Five question")
+		fmt.Print("\n 7- Third question \n 8- Fourth question\n 9- Five question \n 10- Five question\n")
+		fmt.Scan(&questionChoice) // Read user's choice
+		clearScreen()             // Clear the screen
+		switch questionChoice {
+		case 1:
+			questions02.One()
+		case 2:
+			//questions02.Two()
+		case 3:
+			//questions02.Three()
+		case 4:
+			//questions02.Four()
+		case 5:
+			//questions02.Five()
+		case 6:
+			//questions02.Six()
+		case 7:
+			//questions02.Seven()
+		default:
+			fmt.Println("Invalid option!")
+		}
 	}
+}
+
+// clearScreen clears the terminal screen.
+func clearScreen() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
